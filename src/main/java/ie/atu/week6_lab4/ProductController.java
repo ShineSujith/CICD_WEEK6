a167af48ff98d0a8354810c9c8e11c4f1be5a36c
Shine Sujith <G00414454@atu.ie>
2024-10-23 14:30:33 +0100

    created class ProductService

--- src/main/java/ie/atu/week6_lab4/ProductController.java
+++ src/main/java/ie/atu/week6_lab4/ProductController.java
@@ -10,9 +10,15 @@ import java.util.List;
 @RestController
 public class ProductController {
     private List<Product> myList = new ArrayList<>();
+    private ProductService myProduct;
+
+    public ProductController(ProductService myProduct) {
+        this.myProduct = myProduct;
+    }
 
     @PostMapping("/newProduct")
     public List<Product> newProduct(@RequestBody Product product){
+        myList = myProduct.addProduct(product);
         //logic to add new product to database
 
         //return list of products
