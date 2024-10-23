c9acfcb7187509fa10bb128f538a69e76f4c896c
Shine Sujith <G00414454@atu.ie>
2024-10-23 14:11:00 +0100

    project created

---
+++ src/main/java/ie/atu/week6_lab4/Week6Lab4Application.java
@@ -0,0 +1,13 @@
+package ie.atu.week6_lab4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week6Lab4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Week6Lab4Application.class, args);
+	}
+
+}
