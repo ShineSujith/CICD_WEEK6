de24f01d92292a2b49f8e7e6427527ae83cae11c
Shine Sujith <G00414454@atu.ie>
2024-10-23 14:13:49 +0100

    created class product

---
+++ src/main/java/ie/atu/week6_lab4/Product.java
@@ -0,0 +1,14 @@
+package ie.atu.week6_lab4;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+    private String name;
+    private int id;
+    private double price;
+}
